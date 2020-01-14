bfc4afb3841e2e846c6dff5ee415c249c1dd0ab2
michaldobiezynski <dobiezynski@gmail.com>
2020-01-14 17:34:33 +0000

    added redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Users from './user/pages/Users';
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
       <Route path='/'>
         <Users />
       </Route>
+      <Redirect to='/' />
     </Router>
   );
 }
