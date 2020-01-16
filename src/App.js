8e5c5043272aaefd8f996af1a2462308300e6b09
michaldobiezynski <dobiezynski@gmail.com>
2020-01-16 18:58:16 +0000

    changed double qoutes to single qoutes and made the myplaces link and page work

--- src/App.js
+++ src/App.js
@@ -3,34 +3,34 @@ import {
   BrowserRouter as Router,
   Route,
   Redirect,
-  Switch,
+  Switch
 } from 'react-router-dom';
 
 import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlaces';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
+import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <MainNavigation />
       <main>
         <Switch>
-          <Route path='/'>
+          <Route path="/" exact>
             <Users />
           </Route>
-          <Route path='/:userId/places'exact>
+          <Route path="/:userId/places" exact>
             <UserPlaces />
           </Route>
-          <Route path='/places/new'>
+          <Route path="/places/new" exact>
             <NewPlace />
           </Route>
-          <Redirect to='/' />
+          <Redirect to="/" />
         </Switch>
       </main>
     </Router>
   );
-}
+};
 
 export default App;
