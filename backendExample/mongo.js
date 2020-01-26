3c87f5d0b9847552436482809f61e2684fccea7a
michaldobiezynski <dobiezynski@gmail.com>
2020-01-26 20:13:57 +0000

    creating a document with mongoDB

--- backendExample/mongo.js
+++ backendExample/mongo.js
@@ -3,7 +3,26 @@ const MongoClient = require('mongodb').MongoClient;
 const url =
   'mongodb+srv://michal123:michal123@devconnector-6tf9d.mongodb.net/products_test?retryWrites=true&w=majority';
 
-const createProduct = async (req, res, next) => {};
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = db.collection('products').insertOne(newProduct);
+  } catch (error) {
+    return res.json({ message: 'Could not store data.' });
+  }
+
+  client.close();
+
+  res.json(newProduct);
+};
 
 const getProducts = async (req, res, next) => {};
 
