1c65974dfa4a5ac5738ab6a4ec0e6ee24bc9bc37
michaldobiezynski <dobiezynski@gmail.com>
2020-02-04 20:52:09 +0000

    installing mongoose and connecting backend to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/user-routes');
@@ -27,4 +28,14 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
-app.listen(5000);
+mongoose
+  .connect(
+    'mongodb+srv://michal123:michal123@devconnector-6tf9d.mongodb.net/places?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch(error => {
+    console.log(error);
+  });
