b1a38d0931ebc07b74bb1d3b2542844c4cb02c85
michaldobiezynski <dobiezynski@gmail.com>
2020-02-11 20:14:52 +0000

    made the validation for password in the frontend to be the same as in the backend

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -8,7 +8,7 @@ import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
-  VALIDATOR_REQUIRE
+  VALIDATOR_REQUIRE,
 } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import { useHttpClient } from '../../shared/hooks/http-hook';
@@ -24,14 +24,14 @@ const Auth = () => {
     {
       email: {
         value: '',
-        isValid: false
+        isValid: false,
       },
       password: {
         value: '',
-        isValid: false
-      }
+        isValid: false,
+      },
     },
-    false
+    false,
   );
 
   const switchModeHandler = () => {
@@ -39,9 +39,9 @@ const Auth = () => {
       setFormData(
         {
           ...formState.inputs,
-          name: undefined
+          name: undefined,
         },
-        formState.inputs.email.isValid && formState.inputs.password.isValid
+        formState.inputs.email.isValid && formState.inputs.password.isValid,
       );
     } else {
       setFormData(
@@ -49,10 +49,10 @@ const Auth = () => {
           ...formState.inputs,
           name: {
             value: '',
-            isValid: false
-          }
+            isValid: false,
+          },
         },
-        false
+        false,
       );
     }
     setIsLoginMode(prevMode => !prevMode);
@@ -68,11 +68,11 @@ const Auth = () => {
           'POST',
           JSON.stringify({
             email: formState.inputs.email.value,
-            password: formState.inputs.password.value
+            password: formState.inputs.password.value,
           }),
           {
-            'Content-Type': 'application/json'
-          }
+            'Content-Type': 'application/json',
+          },
         );
         auth.login(responseData.user.id);
       } catch (err) {}
@@ -84,11 +84,11 @@ const Auth = () => {
           JSON.stringify({
             name: formState.inputs.name.value,
             email: formState.inputs.email.value,
-            password: formState.inputs.password.value
+            password: formState.inputs.password.value,
           }),
           {
-            'Content-Type': 'application/json'
-          }
+            'Content-Type': 'application/json',
+          },
         );
 
         auth.login(responseData.user.id);
@@ -99,41 +99,41 @@ const Auth = () => {
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
-      <Card className="authentication">
+      <Card className='authentication'>
         {isLoading && <LoadingSpinner asOverlay />}
         <h2>Login Required</h2>
         <hr />
         <form onSubmit={authSubmitHandler}>
           {!isLoginMode && (
             <Input
-              element="input"
-              id="name"
-              type="text"
-              label="Your Name"
+              element='input'
+              id='name'
+              type='text'
+              label='Your Name'
               validators={[VALIDATOR_REQUIRE()]}
-              errorText="Please enter a name."
+              errorText='Please enter a name.'
               onInput={inputHandler}
             />
           )}
           <Input
-            element="input"
-            id="email"
-            type="email"
-            label="E-Mail"
+            element='input'
+            id='email'
+            type='email'
+            label='E-Mail'
             validators={[VALIDATOR_EMAIL()]}
-            errorText="Please enter a valid email address."
+            errorText='Please enter a valid email address.'
             onInput={inputHandler}
           />
           <Input
-            element="input"
-            id="password"
-            type="password"
-            label="Password"
-            validators={[VALIDATOR_MINLENGTH(5)]}
-            errorText="Please enter a valid password, at least 5 characters."
+            element='input'
+            id='password'
+            type='password'
+            label='Password'
+            validators={[VALIDATOR_MINLENGTH(6)]}
+            errorText='Please enter a valid password, at least 6 characters.'
             onInput={inputHandler}
           />
-          <Button type="submit" disabled={!formState.isValid}>
+          <Button type='submit' disabled={!formState.isValid}>
             {isLoginMode ? 'LOGIN' : 'SIGNUP'}
           </Button>
         </form>
