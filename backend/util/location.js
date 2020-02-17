4aee85973ebb9ee5b91dd638a1c21945f15bbd5d
michaldobiezynski <dobiezynski@gmail.com>
2020-02-17 20:50:42 +0000

    using environment variables (Node Js)

--- backend/util/location.js
+++ backend/util/location.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = 'AIzaSyBgQKw_9hpdtO261cpR3IDWwhRgTdpNBx8';
+const API_KEY = process.env.GOOGLE_API_KEY;
 
 async function getCoordsForAddress(address) {
   // return {
@@ -11,8 +11,8 @@ async function getCoordsForAddress(address) {
   // };
   const response = await axios.get(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-      address
-    )}&key=${API_KEY}`
+      address,
+    )}&key=${API_KEY}`,
   );
 
   const data = response.data;
@@ -20,7 +20,7 @@ async function getCoordsForAddress(address) {
   if (!data || data.status === 'ZERO_RESULTS') {
     const error = new HttpError(
       'Could not find location for the specified address.',
-      422
+      422,
     );
     throw error;
   }
